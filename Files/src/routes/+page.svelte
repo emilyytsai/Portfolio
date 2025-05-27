b7129dd2f1abd3805e6161aa7cca8edb63f0d907
emilyytsai <emilytsai@csu.fullerton.edu>
2025-05-27 01:23:28 -0700

    Cleaned up previous tailwind install

--- Files/src/routes/+page.svelte
+++ Files/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-4xl text-purple-600 font-bold">
+  testing testing 😋
+</h1>
