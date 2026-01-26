6c4c4ee1fea4aef52073a216579759741a6fd369
emilyytsai <emilytsai@csu.fullerton.edu>
2026-01-26 05:35:37 -0800

    Added galaxy screen + cursor effect

--- Files/src/routes/+page.svelte
+++ Files/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { slide } from 'svelte/transition';
   import Aurora from '$lib/aurora.svelte';
+  import Galaxy from '$lib/galaxy.svelte';
 
   let showIntro = true;
 
@@ -20,7 +21,7 @@
 {#if showIntro}
   <section
     class="fixed inset-0 bg-black flex items-center justify-center z-50"
-    out:slide={{ duration: 1000 }}
+    out:slide={{ duration: 800 }}
   >
     <div class="absolute inset-0 z-0">
       <Aurora
@@ -51,9 +52,19 @@
         speed={0.1}
       />
     </div>
-    
+    <div class="fixed inset-0 z-10">
+      <Galaxy
+        focal={[0.5, 0.5]}
+        starSpeed={0.5}
+        density={1.4}
+        glowIntensity={0.55}
+        transparent={true}
+        mouseInteraction={true}
+      />
+    </div>
+
     <!-- navbar -->
-    <nav class="fixed top-0 left-0 w-full bg-white/10 backdrop-blur-md z-20">
+    <nav class="fixed top-0 left-0 w-full bg-white/10 backdrop-blur-md z-30">
       <div class="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
         <div class="font-outfit text-3xl font-semibold">Emily Tsai</div>
         <ul class="flex space-x-6 text-2xl font-outfit text-white">
@@ -65,7 +76,7 @@
       </div>
     </nav>
 
-    <div class="font-outfit relative z-10 max-w-2xl mx-auto py-20 space-y-2">
+    <div class="font-outfit relative z-20 max-w-2xl mx-auto py-20 space-y-2">
       <h2 class="text-6xl font-bold text-white text-center">Hello 🌎, I'm Emi!</h2>
       <p class="text-3xl text-white opacity-75 text-center">Aspiring Game Developer & Software Engineer 💗</p>
 
