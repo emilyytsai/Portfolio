a3f226a2ef762cf854a459c624b201d669552d94
Emily Tsai <emilytsai@csu.fullerton.edu>
2026-04-14 14:00:16 -0700

    Updated page with photos, updated resume, and added Communications minor

--- Files/src/routes/+page.svelte
+++ Files/src/routes/+page.svelte
@@ -139,7 +139,7 @@
   <section id="about" class="font-outfit relative z-20 max-w-2xl mx-auto px-4 py-6 space-y-1 sm:space-y-3 lg:space-y-5">
     <h4 class="text-3xl sm:text-4xl md:text-5xl font-bold text-white text-center pt-6">About Me</h4>
     <p class="text-base sm:text-lg lg:text-xl text-white text-center py-2">
-      I am a <u>Computer Science Major, with a Minor in Data Science</u> @ <u>CSU Fullerton!</u><br><br>
+      I am a <u>Computer Science Major, with a Minor in Data Science and a Minor in Communication Studies</u> @ <u>CSU Fullerton!</u><br><br>
 
       I currently serve as the <b>President</b> of CSUF's 
       <a href="https://vgdccsuf.com/" target="_blank" rel="noopener noreferrer" style="color: #6c80d9;" class="hover:underline font-bold">Video Game Development Club (VGDC),</a>
@@ -302,6 +302,15 @@
     </p>
 
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 sm:gap-3 lg:gap-4 mx-auto">
+      <div class="relative w-full aspect-[4/3] border border-white/15 rounded-2xl shadow-2xl overflow-hidden hover:scale-105 transition">
+        <img src="/28.png" alt="image" class="w-full h-full object-cover"/>
+      </div>
+
+      <div class="relative w-full aspect-[4/3] border border-white/15 rounded-2xl shadow-2xl overflow-hidden hover:scale-105 transition">
+        <img src="/27.png" alt="image" class="w-full h-full object-cover"/>
+      </div>
+      
+      
       <div class="relative w-full aspect-[4/3] border border-white/15 rounded-2xl shadow-2xl overflow-hidden hover:scale-105 transition">
         <img src="/26.png" alt="image" class="w-full h-full object-cover"/>
       </div>
