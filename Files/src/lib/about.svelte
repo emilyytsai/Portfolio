a760145a5069af42e1630a033576ec245addf126
emilyytsai <emilytsai@csu.fullerton.edu>
2026-04-23 01:19:43 -0700

    Adjusted about me section

--- Files/src/lib/about.svelte
+++ Files/src/lib/about.svelte
@@ -2,7 +2,9 @@
   <section id="about" class="font-outfit relative z-20 max-w-2xl mx-auto px-4 py-6 space-y-1 sm:space-y-3 lg:space-y-5">
     <h4 class="text-3xl sm:text-4xl md:text-5xl font-bold text-white text-center pt-6">About Me</h4>
     <p class="text-base sm:text-lg lg:text-xl text-white text-center py-2">
-      I am a <u>Computer Science Major, with a Minor in Data Science and a Minor in Communication Studies</u> @ <u>CSU Fullerton!</u><br><br>
+      I am a <u style="color: #98aafa;"><b style="color: #98aafa;">Computer Science</b> Major</u>, with a <u style="color: #98aafa;">
+        Minor in <b style="color: #98aafa;">Data Science</b></u> and a <u style="color: #98aafa;">
+        Minor in <b style="color: #98aafa;">Communcation Studies</b></u> @ <u>CSU Fullerton!</u><br><br>
 
       I currently serve as the <b>President</b> of CSUF's 
       <a href="https://vgdccsuf.com/" target="_blank" rel="noopener noreferrer" style="color: #6c80d9;" class="hover:underline font-bold">Video Game Development Club (VGDC),</a>
